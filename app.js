4327653bcbe59e88a6cef0b0245302a4e4b7389b
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-09-25 00:10:17 +0200

    Only show preview of poems on home page

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const compression = require('compression');
 const fs = require("fs");
 const path = require("path");
+const crypto = require('crypto');
 const Firestore = require("@google-cloud/firestore");
 const db = new Firestore({
   projectId: process.env.GCLOUD_PROJECT_ID,
     keyFilename: "/keyfile.json",
   });
-const { randomInt } = require("crypto");
 
 const port = 3000;
 app.use(compression());
@@ -19,8 +19,11 @@ const poemPath = path.join(__dirname, "views", "poem.html");
 const notFoundPath = path.join(__dirname, "views", "404.html");
 
 app.use((req, res, next) => {
+  const nonce = crypto.randomBytes(16).toString('base64');
+  res.locals.nonce = nonce;
+
   res.set("Cache-Control", "no-cache, public");
-  res.set("Content-Security-Policy", "default-src 'none'; script-src 'self' 'strict-dynamic' 'sha256-+WsLtgSAA4V5nkvNBFCUwS8SLpM5QoLyEkh1rP/64eo=' 'unsafe-inline' https: http:; style-src 'self'; connect-src 'self'; img-src 'self'; base-uri 'none'; require-trusted-types-for 'script';");
+  res.set("Content-Security-Policy", `default-src 'none'; script-src 'self' 'strict-dynamic' 'unsafe-inline' 'nonce-${nonce}' https: http:; style-src 'self'; connect-src 'self'; img-src 'self'; base-uri 'none'; require-trusted-types-for 'script';`);
   res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   res.set("X-Content-Type-Options", "nosniff");
   res.set("X-Frame-Options", "DENY");
@@ -32,13 +35,16 @@ app.use((req, res, next) => {
 });
 
 app.get("/data/poem/random", async (req, res) => {
-  const number = randomInt(0,2);
+  const number = crypto.randomInt(0,2);
   const snapshot = await db.collection("poems").where("index", "==", number).get();
   if (snapshot.empty) {
     return res.sendStatus(500);
   }  
   snapshot.forEach(doc => {
-    res.json(doc.data());
+    let data = doc.data();
+    const poem = data.poem.split('\n').slice(0, 13).join('\n');
+    data.poem = poem
+    res.json(data);
   });
 });
 
@@ -86,6 +92,7 @@ app.get("/", async (req, res) => {
       }
   
       let modifiedHtml = indexData.replace("{{footer}}", footerData);
+      modifiedHtml = modifiedHtml.replace("{{nonce}}", res.locals.nonce);
   
       res.send(modifiedHtml);
     });
